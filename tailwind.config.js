fa2248eb03fa6fa4365a66c9a0d2e5ac57f8155f
olga <olga.goryszewska@gmail.com>
2024-06-11 23:57:05 +0200

    Update file paths and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D8D7D7",
+        black: "#252323",
+        brown: "#CB845C",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
