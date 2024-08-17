0928d5d416f5a120b8092678ae215fda60cba6d7
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2024-08-17 21:31:43 +0200

    installing prettier and configuration

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-
-
   return (
     <>
-    <h1>hello</h1>
+      <h1>hello</h1>
     </>
-  )
+  );
 }
-export default App
+export default App;
