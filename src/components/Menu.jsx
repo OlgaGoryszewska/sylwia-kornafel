5ed319d123d4d3e14908921eb8f65595f9d5d1c3
olga <olga.goryszewska@gmail.com>
2024-10-01 13:26:16 +0200

    adding animation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
+import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
+gsap.registerPlugin(ScrambleTextPlugin);
 
 import logo from '../assets/img/menuLogo.png';
 
 export const Menu = () => {
   const [showMenu, setShowMenu] = useState(false);
   const logoRef = useRef(null);
+  const menuRef = useRef(null);
+  const linkRef = useRef(null);
 
   const handleClickAnimation = () => {
     gsap.fromTo(
@@ -17,9 +21,18 @@ export const Menu = () => {
     setShowMenu(!showMenu);
   };
 
+  useEffect(() => {
+    gsap.to(linkRef.current, {
+      duration: 1,
+      scrambleText: { text: 'Home' },
+      chars: 'X0',
+      ease: 'power3',
+    });
+  });
+
   return (
     <div className="flex flex-col max-w-full ">
-      <button className="flex flex-row justify-end">
+      <button ref={menuRef} className="flex flex-row justify-end">
         <img
           alt="menu"
           src={logo}
@@ -28,9 +41,13 @@ export const Menu = () => {
           onClick={handleClickAnimation}
         />
       </button>
-      <ul className="flex flex-col mx-auto">
-        <li>{showMenu && <Link to="/">Home</Link>}</li>
-      </ul>
+      {showMenu && (
+        <ul className="flex flex-col mx-auto">
+          <li ref={linkRef}>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
