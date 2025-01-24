8aa3dc45ff637297000baf5d7e2c634df9e2ed1c
olga goryszewska <olga.goryszewska@gmail.com>
2025-01-24 06:38:27 +0530

    adding settings

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,61 +1,57 @@
-import Face from '../assets/img/faceNoiceHorisontal.jpg';
-import { Nav } from '../components/Nav';
-import { AboutUs } from '../components/AboutUs';
 import React, { useEffect, useRef } from 'react';
+import { SplitText } from 'gsap/SplitText';
 import { gsap } from 'gsap';
+import Face from '../assets/img/faceTwo.png';
+import { Nav } from '../components/Nav';
+
+gsap.registerPlugin(SplitText); // register the plugin
 
 const HomePage = () => {
-  const textRef1 = useRef(null); // Reference for the first text element
-  const textRef2 = useRef(null); // Reference for the second text element
+  const textRef = useRef(null);
 
   useEffect(() => {
-    const textWidth = textRef1.current.offsetWidth; // Get the width of the text element
-
-    // Animate both text elements to move continuously
-    gsap.fromTo(
-      [textRef1.current, textRef2.current],
-      { x: 0 }, // Start position
-      {
-        x: `-${textWidth}px`, // End position (move by the width of the text)
-        duration: 45, // Duration of one full cycle
-        repeat: -1, // Infinite loop
-        ease: 'linear', // Linear movement (constant speed)
-      }
-    );
+    const split = new SplitText(textRef.current, { type: 'chars' });
+    gsap.from(split.chars, {
+      duration: 1,
+      y: 50,
+      opacity: 0,
+      rotationX: 70,
+      transformOrigin: '0% 50% -50',
+      ease: 'back',
+      stagger: 0.01,
+    });
   }, []);
 
   return (
     <>
-      <Nav className="absolute" />
-      <div className="overflow-hidden w-full relative">
-        <div className="absolute flex whitespace-nowrap z-20 mt-40 ">
-          {/* First text element */}
-          <p
-            ref={textRef1}
-            className="font-bochanstyled text-gold text-5xl inline-block text-shadow-lg"
-          >
-            &nbsp;• PMU • Podologia • Kosmetyka • Zabiegi Na Twarz • Lasery •
-            PMU • Podologia • Kosmetyka • Zabiegi Na Twarz • Lasery • PMU •
-            Podologia • Kosmetyka • Zabiegi Na Twarz • Lasery&nbsp;
+      <Nav className=" absolute" />
+      <img
+        className="relative hidden"
+        src={Face}
+        alt="bialy lotos ciechanow main picture of a women face
+        "
+      />
+      <div className=" flex flex-col px-8 pt-12 ">
+        <div ref={textRef} className="card">
+          <h1 className="text-lg font-dmserif font-black">
+            Nowa Strona Internetowa w Budowie
+          </h1>
+          <p className="pb-2">Witamy na naszej nowej stronie internetowej!</p>
+          <p className="pb-2">
+            Z radością informujemy, że Salon Biały Lotos pracuje nad
+            uruchomieniem nowej platformy.
           </p>
-          {/* Second text element (duplicate of the first) */}
-          <p
-            ref={textRef2}
-            className="font-bochanstyled text-gold text-5xl inline-block text-shadow-lg"
-          >
-            &nbsp;• PMU • Podologia • Kosmetyka • Zabiegi Na Twarz • Lasery •
-            PMU • Podologia • Kosmetyka • Zabiegi Na Twarz • Lasery • PMU •
-            Podologia • Kosmetyka • Zabiegi Na Twarz • Lasery&nbsp;
+          <p className="pb-2">
+            Chcemy, aby była ona intuicyjna, nowoczesna i pełna inspiracji dla
+            naszych klientów. Obecnie strona jest w trakcie budowy, ale
+            zapraszamy do zapoznania się z naszymi ofertami na listopad!
+          </p>
+          <p>
+            Poniżej znajdziesz najnowsze propozycje, które przygotowaliśmy
+            specjalnie dla Ciebie.
           </p>
         </div>
-        <img
-          className="relative object-cover
-          "
-          src={Face}
-          alt="bialy lotos ciechanow main picture of a women face"
-        />
       </div>
-      <AboutUs />
     </>
   );
 };
